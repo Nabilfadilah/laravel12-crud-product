f3dc7b8f01bb6e71337ddfc1fe5e659037c35031
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-22 20:50:44 +0700

    Membuat views layouts project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Product CRUD</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-5">
+        @yield('content')
+    </div>
+</body>
+
+</html>
