a6c3c072a60f27b536eb0e20bf6e0669ca74f157
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-22 21:44:27 +0700

    Menambah fillable user_id dan relas table producti ke table user

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'price', 'description', 'image'];
+    protected $fillable = ['name', 'price', 'description', 'image', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
