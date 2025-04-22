cd2aab38908025d674a3247f5599596673f689e6
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-22 21:42:32 +0700

    Menambah route register, login, logout dan middleware uth

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return redirect()->route('products.index');
+Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('register', [AuthController::class, 'register']);
+
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('products', ProductController::class);
 });
 
-Route::resource('products', ProductController::class);
+
+// Route::get('/', function () {
+//     return redirect()->route('products.index');
+// });
+
+// Route::resource('products', ProductController::class);
