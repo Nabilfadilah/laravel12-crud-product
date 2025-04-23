f7262208c935fa2d5d2edff554237207bb76f1f2
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-23 12:01:48 +0700

    Merubah ukuran button dan text product list

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="d-flex justify-content-between align-items-center mb-4">
-        <h2 class="fw-bold">Product List</h2>
-        <a href="{{ route('products.create') }}" class="btn btn-primary">+ Add Product</a>
+        <h4 class="fw-bold">Product List</h4>
+        <a href="{{ route('products.create') }}" class="btn btn-sm btn-primary">+ Add Product</a>
     </div>
 
     @if (session('success'))
