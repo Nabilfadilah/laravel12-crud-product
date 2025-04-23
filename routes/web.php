58c4886510fccee2a4121adeeea9d33b43427e1b
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-23 10:44:21 +0700

    Menambah route profile, serta users dengan middleware role admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('register', [AuthController::class, 'register']);
@@ -14,6 +15,15 @@ Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::resource('products', ProductController::class);
+
+    Route::get('/profile', [UserController::class, 'profile'])->name('users.profile');
+
+    // Hanya admin
+    Route::middleware('role:admin')->group(function () {
+        Route::resource('users', UserController::class)->except(['show']);
+        Route::get('/users/{user}/reset-password', [UserController::class, 'showResetPasswordForm'])->name('users.resetPasswordForm');
+        Route::post('/users/{user}/reset-password', [UserController::class, 'resetPassword'])->name('users.resetPassword');
+    });
 });
 
 
