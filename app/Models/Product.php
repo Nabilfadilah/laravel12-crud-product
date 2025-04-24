ab3a2de55fbc29e20f076c5de0e69886f218528e
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-24 14:25:51 +0700

    Memberikan komen penjelasan untuk model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    // field yang boleh diisi secara mass-assignment
     protected $fillable = ['name', 'price', 'description', 'image', 'user_id'];
 
+    // relasi ke table users
     public function user()
     {
         return $this->belongsTo(User::class);
