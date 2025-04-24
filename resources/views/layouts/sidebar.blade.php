b2f71c1e3d7d67ba5e2b4ddcede818c01762ca8c
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-24 14:31:28 +0700

    Memberikan komen penjelasan untuk view layout sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,12 @@
 <ul class="nav flex-column">
+    <!-- link ke halaman Profile -->
     <li class="nav-item mb-2">
         <a href="{{ route('users.profile') }}" class="nav-link {{ request()->is('profile*') ? 'active' : '' }}">
             <i class="bi bi-person me-2"></i> Profile
         </a>
     </li>
 
+    <!-- link ke halaman Products, hanya muncul jika user role-nya 'user' -->
     @if (auth()->user()->role === 'user')
         <li class="nav-item mb-2">
             <a href="{{ route('products.index') }}" class="nav-link {{ request()->is('products*') ? 'active' : '' }}">
@@ -13,6 +15,7 @@
         </li>
     @endif
 
+    <!-- link ke halaman Users, hanya muncul jika role-nya 'admin' -->
     @if (auth()->user()->role === 'admin')
         <li class="nav-item mb-2">
             <a href="{{ route('users.index') }}" class="nav-link {{ request()->is('users*') ? 'active' : '' }}">
